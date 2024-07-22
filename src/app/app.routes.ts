4752ec81aa116eaf2f87ef738c7c5a2453aacc94
Christian Guevara <chrisdb150@gmail.com>
2024-07-22 17:38:46 -0400

    init config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,5 +10,13 @@ export const routes: Routes = [
         path: "home",
         loadComponent: () => import("./modules/example-page/example-page.component").then((m) => m.ExamplePageComponent)
 
+    }, {
+        path: "login",
+        loadComponent: () => import("./modules/auth/login/login.component").then((m) => m.LoginComponent)
+
+    }, {
+        path: "tasks",
+        loadComponent: () => import("./modules/tasks/tasks.component").then((m) => m.TasksComponent)
+
     }
 ];
